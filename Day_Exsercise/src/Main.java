70b111c10bf6243868c567ec966e747c1ebab955
miserydx <miserydx@163.com>
2021-02-19 11:51:17 +0800

    week4 update

--- Day_Exsercise/src/Main.java
+++ Day_Exsercise/src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("dsadasdssss");
+        int first = 127;
+        byte type = (byte) (first >> 4);
+//        byte type = (byte) (first);
+        System.out.println("type = " + type);
+        byte b = 21;
+        int i = (int) b;
+        System.out.println("i = " + i);
     }
 
 }
